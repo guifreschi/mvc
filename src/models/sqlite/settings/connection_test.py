54a3e1225255c0e65a1b7d6f19588880b71d5192
Guilherme Freschi <guilhermefreschix@gmail.com>
2025-02-06 20:19:01 -0300

    feat: Implementing pets_repository with first test

--- src/models/sqlite/settings/connection_test.py
+++ src/models/sqlite/settings/connection_test.py
@@ -6,7 +6,7 @@ from .connection import db_connection_handler
 def test_connect_db():
     assert db_connection_handler.get_engine() is None
 
-    db_connection_handler.connect_db()
+    db_connection_handler.connect_to_db()
     db_engine = db_connection_handler.get_engine()
 
     assert db_engine is not None
