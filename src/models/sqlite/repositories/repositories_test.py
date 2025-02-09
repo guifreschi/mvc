62401d023a4a552bfd589ef1bcdbbcc777c6a7ed
Guilherme Freschi <guilhermefreschix@gmail.com>
2025-02-09 16:16:49 -0300

    test: Finishing tests in pets_repository

--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,6 +1,6 @@
 import pytest
 from src.models.sqlite.settings.connection import db_connection_handler
-from .pet_repository import PetsRepository
+from .pets_repository import PetsRepository
 
 # db_connection_handler.connect_to_db()
 
